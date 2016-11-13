58e383107d075325575f62155560e97b769814fe
Soren H. Pedersen <speder13@student.aau.dk>
2016-11-13 14:59:41 +0100

    update

--- MI_Test_UI/Form1.Designer.cs
+++ MI_Test_UI/Form1.Designer.cs
@@ -28,12 +28,232 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBoxOrig = new System.Windows.Forms.PictureBox();
+            this.pictureBoxProcessed = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.progressBar4 = new System.Windows.Forms.ProgressBar();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.progressBar5 = new System.Windows.Forms.ProgressBar();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOrig)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxProcessed)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxOrig
+            // 
+            this.pictureBoxOrig.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxOrig.Name = "pictureBoxOrig";
+            this.pictureBoxOrig.Size = new System.Drawing.Size(400, 200);
+            this.pictureBoxOrig.TabIndex = 0;
+            this.pictureBoxOrig.TabStop = false;
+            // 
+            // pictureBoxProcessed
+            // 
+            this.pictureBoxProcessed.Location = new System.Drawing.Point(12, 233);
+            this.pictureBoxProcessed.Name = "pictureBoxProcessed";
+            this.pictureBoxProcessed.Size = new System.Drawing.Size(400, 200);
+            this.pictureBoxProcessed.TabIndex = 1;
+            this.pictureBoxProcessed.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(440, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(280, 469);
+            this.textBox1.TabIndex = 2;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(841, 9);
+            this.progressBar1.Maximum = 1307568;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(727, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Load Colors to Ignore";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(727, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Load Image Source";
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(841, 69);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(100, 23);
+            this.progressBar2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(727, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Compare Colors";
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(841, 98);
+            this.progressBar3.Maximum = 80000;
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(100, 23);
+            this.progressBar3.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(727, 130);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Create New Image";
+            // 
+            // progressBar4
+            // 
+            this.progressBar4.Location = new System.Drawing.Point(841, 127);
+            this.progressBar4.Maximum = 80000;
+            this.progressBar4.Name = "progressBar4";
+            this.progressBar4.Size = new System.Drawing.Size(100, 23);
+            this.progressBar4.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(956, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "label5";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 475);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Next Image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(956, 40);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(727, 43);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(113, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Load Image Filenames";
+            // 
+            // progressBar5
+            // 
+            this.progressBar5.Location = new System.Drawing.Point(841, 40);
+            this.progressBar5.Name = "progressBar5";
+            this.progressBar5.Size = new System.Drawing.Size(100, 23);
+            this.progressBar5.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(956, 69);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 13);
+            this.label8.TabIndex = 16;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(115, 480);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "label9";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1083, 510);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.progressBar5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.progressBar4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBoxProcessed);
+            this.Controls.Add(this.pictureBoxOrig);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOrig)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxProcessed)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxOrig;
+        private System.Windows.Forms.PictureBox pictureBoxProcessed;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ProgressBar progressBar4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ProgressBar progressBar5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
     }
 }
 
--- MI_Test_UI/Form1.cs
+++ MI_Test_UI/Form1.cs
@@ -7,14 +7,119 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using ProcessImage;
 
 namespace MI_Test_UI
 {
     public partial class Form1 : Form
-    {
+    {       
+        List<Int32> colorList = new List<Int32>();
+        Dictionary<int, int> ignoreColors = new Dictionary<int, int>();
+        List<Int32> imageColors = new List<Int32>();
+        List<Int32> newimageColors;
+        string[] imageFiles;
+        Int32 hvid = Color.White.ToArgb();
+        Color[] ignores = new Color[80000];
+        Color[] pixels;
+        Color[] newImage;
+        int l = 0;
+        int g = 0;
+        int h = 0;
+        int filesLoaded = 0;
+        int imageCounter = 0;
+        ProcessImage.ImageProcessor imageprocessor = new ImageProcessor();
         public Form1()
         {
             InitializeComponent();
+            load_ignore_values();
+            load_image_files();
+                
+        }
+        public void load_ignore_values()
+        {
+            string file = "SavedList.txt";
+            using (StreamReader r = new StreamReader(file)){
+                string line;
+                while ((line = r.ReadLine()) != null){
+                    ignoreColors.Add(Int32.Parse(line), Int32.Parse(line));
+                    progressBar1.Increment(1);
+                }
+            }
+            
+            label5.Text = (ignoreColors.Count + " Colors Loaded.");
+        }
+        
+        public void load_image_files()
+        {
+            g = 0;
+            imageFiles = Directory.GetFiles(".\\images", "*.png");
+            progressBar5.Increment(100);
+            label6.Text = ("All Files Loaded.");
+            foreach (string fil in imageFiles)
+            {
+                g++;
+            }
+            filesLoaded = g;
+            label9.Text = "0 / " + filesLoaded;
+        }
+
+        public void load_image_from_file(string filename)
+        {
+            pixels = null;
+            imageColors.Clear();
+            progressBar2.Value = 0;
+            System.Drawing.Image image = System.Drawing.Image.FromFile(filename);
+            pictureBoxOrig.Image = image;
+            pixels = imageprocessor.convertToPixels(image);
+            foreach (Color farve in pixels){
+                imageColors.Add(farve.ToArgb());
+            }
+            progressBar2.Increment(100);
+            label8.Text =("File Loaded");
+        }
+
+        public void compare_images()
+        {
+            progressBar3.Value = 0;
+            newimageColors = null;
+            newimageColors = new List<Int32>();
+            progressBar3.Value = 0;
+            h = -1;
+            foreach (Int32 farv in imageColors){
+                h++;
+                if (ignoreColors.ContainsKey(farv)){
+                    newimageColors.Add(hvid);
+                }                
+                else{
+                    newimageColors.Add(farv);
+                }
+                progressBar3.Increment(1);
+            }
+        }
+
+        public void create_new_image()
+        {
+            progressBar4.Value = 0;
+            newImage = null;
+            newImage = new Color[80000];
+            l = 0;
+            foreach (Int32 color in newimageColors){
+                newImage[l] = Color.FromArgb(color);
+                l++;
+                progressBar4.Increment(1);
+            }
+            l = 0;
+            pictureBoxProcessed.Image = imageprocessor.convertToImage(newImage);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            load_image_from_file(imageFiles[imageCounter]);
+            compare_images();
+            create_new_image();
+            imageCounter++;
+            label9.Text = (imageCounter + " / " + filesLoaded);
         }
     }
 }
